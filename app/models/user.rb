fe7a9e0b987d2bea7c2e881dc3888ea089b7f111
zusaaaa <obbyokamoto@gmail.com>
2020-07-15 22:45:44 +0900

    [WIP]groups-model_
    グループモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable,  :validatable
 
   validates :name, presence: true, uniqueness: true
-
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
