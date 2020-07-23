bc405cfbdb46ebda33b5c9408ea2782bf636ec32
zusaaaa <obbyokamoto@gmail.com>
2020-07-23 16:53:52 +0900

    asynchronous_processing_js
    非同期通信機能の実装

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ChatSpace
       g.helper false
       g.test_framework false
       config.i18n.default_locale = :ja
+      config.time_zone = 'Tokyo'
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
