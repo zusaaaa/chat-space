e16480ed4436a37f6f811db36a108db7919691e7
zusaaaa <obbyokamoto@gmail.com>
2020-07-24 14:44:03 +0900

    auto-update
    自動更新機能の実装

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="message-box">
+        `<div class="message-box" data-message-id=${message.id}>
           <div class="message-list">
             <div class="message-list__name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="message-box">
+      `<div class="message-box" data-message-id=${message.id}>
         <div class="message-list">
           <div class="message-list__name">
             ${message.user_name}
@@ -59,8 +59,39 @@ $(function(){
       $('form')[0].reset();
       $('.form__send').prop('disabled', false);
     })
-    .fail(function(){
+    .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.form__send').prop('disabled', false);
     });
   });
+
+  // let reloadMessages = function () {
+  //   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  //   let last_message_id = $('.message-box:last').data("message-id") || 0;
+  //   $.ajax({
+  //     //ルーティングで設定した通りhttpメソッドをgetに指定
+  //     type: 'get',
+  //     dataType: 'json',
+  //     //dataオプションでリクエストに値を含める
+  //     data: { id: last_message_id }
+  //   })
+  //     .done(function (messages) {
+  //       // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+  //       if (messages.length !== 0) {
+  //         //追加するHTMLの入れ物を作る
+  //         let insertHTML = '';
+  //         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+  //         $.each(messages, function (i, message) {
+  //           insertHTML += buildHTML(message)
+  //         });
+  //         //メッセージが入ったHTMLに、入れ物ごと追加
+  //         $('.main').append(insertHTML);
+  //         $('.main').animate({ scrollTop: $('.message-box')[0].scrollHeight });
+  //       }
+  //     })
+  //     .fail(function () {
+  //       alert('error');
+  //     });
+  //   setInterval(reloadMessages, 7000);
+  // };
 });
